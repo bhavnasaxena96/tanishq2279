aac10f6cef8ea4e3ba0d574f090d5a59cd4eb168
bhavnasaxena96 <55221626+bhavnasaxena96@users.noreply.github.com>
2020-10-01 16:10:09 +0530

    Update multiplication.c

--- multiplication.c
+++ multiplication.c
@@ -1,29 +1,29 @@
 #include<stdio.h>
 void main()
 {
-int a[2][2],B[2][2],C[2][2],i,j,k;
+int a[3][3],B[3][3],C[3][3],i,j,k;
 printf("Enter Matrix A:");
-for(i=0;i<2;i++)
+for(i=0;i<3;i++)
     {
-     for(j=0;j<2;j++)
+     for(j=0;j<3;j++)
         {
          scanf("%d",&a[i][j]);
         }
     }
 printf("Enter Matrix B:");
-for(i=0;i<2;i++)
+for(i=0;i<3;i++)
     {
-    for(j=0;j<2;j++)
+    for(j=0;j<3;j++)
         {
          scanf("%d",&B[i][j]);
         }
     }
-for(i=0;i<2;i++)
+for(i=0;i<3;i++)
     {
-    for(j=0;j<2;j++)
+    for(j=0;j<3;j++)
         {
             C[i][j]=0;
-            for(k=0;k<2;k++)
+            for(k=0;k<3;k++)
             {
                 C[i][j] += (a[i][k]*B[k][j]);
 
@@ -31,9 +31,9 @@ for(i=0;i<2;i++)
         }
     }
 printf("Multiplication : ");
-for(i=0;i<2;i++)
+for(i=0;i<3;i++)
     {
-     for(j=0;j<2;j++)
+     for(j=0;j<3;j++)
           {
            printf("%d ",C[i][j]);
           }
